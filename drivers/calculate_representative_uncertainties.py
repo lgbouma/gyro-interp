055b47c83f61e502fa035c1d2827ce57a0c73968
lgbouma <bouma.luke@gmail.com>
2022-11-29 08:59:20 -0800

    add drivers/calculate_representative_uncertainties

---
+++ drivers/calculate_representative_uncertainties.py
@@ -0,0 +1,25 @@
+"""
+Calculate the uncertainties for repesentative Teffs and ages quoted in the
+abstract, and throughout the manuscript.  Writes to stdout.
+"""
+
+import numpy as np
+from gyroemp import gyro_age_posterior
+from gyroemp.models import slow_sequence
+from gyroemp.helpers import given_grid_post_get_summary_statistics
+
+# Sun-like stars
+age_grid = np.linspace(0, 2600, 100)
+ages = [200, 500, 1500, 2000]
+Teffs, Teff_err = [6000, 5800, 5600, 5500, 5000, 4500, 4200], 50
+for Teff in Teffs:
+    print(42*'-')
+    for age in ages:
+        Prot = slow_sequence(Teff, age)
+        Prot_err = 0.01*Prot
+        age_posterior = gyro_age_posterior(
+          Prot, Teff, Prot_err=Prot_err, Teff_err=Teff_err, age_grid=age_grid
+        )
+        d = given_grid_post_get_summary_statistics(age_grid, age_posterior)
+        print(f"Teff: {Teff}, age: {age} Myr, med: {d['median']:.1f}, "+
+              f"+1sig%: {100*d['+1sigmapct']:.1f}%, -1sig%: {100*d['-1sigmapct']:.1f}%")
