8aa27e932a1ab07b588ca6b2ba6d0be66be1ae2c
lgbouma <bouma.luke@gmail.com>
2022-11-29 09:23:02 -0800

    cdf fast/slow: check alpha Per

--- drivers/plot_cdf_fast_slow_ratio.py
+++ drivers/plot_cdf_fast_slow_ratio.py
@@ -13,5 +13,14 @@ outdir = PLOTDIR
 
 for poly_order in [7]:
     ap.plot_cdf_fast_slow_ratio(
-        outdir, poly_order=poly_order
+        outdir, poly_order=poly_order,
+        model_ids = ['α Per', '120-Myr', '300-Myr', 'Praesepe'],
+        reference_clusters = ['α Per', 'Pleiades', 'Blanco-1', 'Psc-Eri', 'NGC-3532',
+                              'Group-X', 'Praesepe', 'NGC-6811']
+    )
+    ap.plot_cdf_fast_slow_ratio(
+        outdir, poly_order=poly_order,
+        model_ids = ['120-Myr', '300-Myr', 'Praesepe'],
+        reference_clusters = ['Pleiades', 'Blanco-1', 'Psc-Eri', 'NGC-3532',
+                              'Group-X', 'Praesepe', 'NGC-6811']
     )
