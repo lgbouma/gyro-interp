lgbouma/gyro-interp

commit 82d376007bd24bc2bc0638b760d8047ee86d2d56

core: update notation to match ms

drivers/run_emcee_fit_gyro_model.py
@@ -1,5 +1,5 @@
 """
-Run MCMC to get best-fit C, C_y0, k0, k2, f, and their uncertainties.
+Run MCMC to get best-fit a1, y_g, logk0, logk1, logf, and their uncertainties.
 
 Usage:
     Check modelid and grid matches what you want.  Then:
@@ -59,13 +59,13 @@ def log_probability(theta):
 
 def log_prior(theta):
 
-    C, C_y0, logk0, logk2, logf = theta
+    a1, y_g, logk0, logk1, logf = theta
 
     if (
-        1 < C < 20 and
-        0.1 < C_y0 < 1 and
+        1 < a1 < 20 and
+        0.1 < y_g < 1 and
         -10 < logk0 < 0 and
-        -10 < logk2 < 0 and
+        -10 < logk1 < 0 and
         -3 < logf < 3
     ):
         return 0.0
@@ -75,7 +75,7 @@ def log_prior(theta):
 
 def log_likelihood(theta):
 
-    C, C_y0, logk0, logk2, logf = theta
+    a1, y_g, logk0, logk1, logf = theta
 
     likelihoods = np.zeros(len(datasets))
 
@@ -84,14 +84,13 @@ def log_likelihood(theta):
         # get model, given age in Myr
         sigma_period = 0.51
         parameters = {
-            'A': 1,
-            'B': 0,
-            'C': C,
-            'C_y0': C_y0,
-            'logk0': logk0,
-            'logk2': logk2,
-            'l1': -2*sigma_period,
-            'k1': np.pi # a joke, but it works
+            'a0': 1,
+            'a1': sample[0],
+            'y_g': sample[1],
+            'logk0': sample[2],
+            'logk1': sample[3],
+            'l_hidden': -2*sigma_period,
+            'k_hidden': np.pi # a joke, but it works
         }
 
         h_vals_ss, h_vals_fs, teff_midway = _get_model_histogram(
@@ -142,7 +141,7 @@ def main():
         nll = lambda *args: -log_likelihood(*args)
 
         eps = 1e-3
-        #C, C_y0, logk0, logk2, logf = theta
+        #a1, y_g, logk0, logk1, logf = theta
         initial = np.array([8.7, 0.66, -5, -6.2, 1]) + eps*np.random.randn(ndim)
 
         bounds = bounds=(
@@ -152,12 +151,9 @@ def main():
         print('beginning minimization...')
         soln = minimize(nll, initial)
 
-        C_ml, C_y0_ml, k0_ml, k2_ml, f_ml = soln.x
-
         # finds: array([ 8.25637368,  0.6727635 , -4.88458715, -6.23968725, -0.14829159])
-        # or with bounds, array([ 8.68683268,  0.67138394, -4.96275124, -6.20941048, -0.16000444])
         print(f"Got max-likelihood solution:")
-        params = "C, C_y0, logk0, logk2, logf"
+        params = "a1, y_g, logk0, logk1, logf"
         print(params)
         print(soln.x)
         map_soln = soln.x
@@ -235,7 +231,7 @@ def main():
 
     # corner plot
     outpath = os.path.join(outdir, "corner.png")
-    labels = ["C", "C_y0", "logk0", "logk2", "logf"]
+    labels = ["a1", "y_g", "logk0", "logk1", "logf"]
     if not os.path.exists(outpath):
         fig = corner.corner(
             flat_samples, labels=labels

gyrointerp/gyro_posterior.py
@@ -235,10 +235,9 @@ def gyro_age_posterior(
         what ages of reference clusters are correct.  The scale is as described
         in the manuscript, and defined in /gyrointerp/age_scale.py
 
-        popn_parameters (str or dict). (str) "default", or (dict) containing
-        the population-level free parameters.  Keys of "A", "C", "C_y0", "k0",
-        "l1", "k1", and "k2" must all be specified.  If "B" is not specified,
-        assumes B=0, and the optional component mentioned above is omitted.
+        popn_parameters: (str) "default", or (dict) containing the
+        population-level free parameters.  Keys of "a0", "a1", "k0", "k1",
+        "y_g", "l_hidden", and "k_hidden" must all be specified.
 
     Returns:
         * NaN if Teff outside [3800, 6200] K
@@ -381,6 +380,8 @@ def _one_star_age_posterior_worker(task):
 
 def _get_pop_samples(N_pop_samples):
 
+    # TODO: cache this in an accessible way so that 
+    # TODO: all users can use gyro_age_posterior_mcmc
     pklpath = os.path.join(LOCALDIR, "gyrointerp", "fitgyro_emcee_v02",
                            "fit_120-Myr_300-Myr_Praesepe.pkl")
     with open(pklpath, 'rb') as f:
@@ -395,16 +396,15 @@ def _get_pop_samples(N_pop_samples):
     popn_parameter_list = []
     for ix in range(N_pop_samples):
         sample = sel_samples[ix, :]
-        #C, C_y0, logk0, logk2, logf = theta
+        #a1, y_g, logk0, logk1, logf = theta
         parameters = {
-            'A': 1,
-            'B': 0,
-            'C': sample[0],
-            'C_y0': sample[1],
+            'a0': 1,
+            'a1': sample[0],
+            'y_g': sample[1],
             'logk0': sample[2],
-            'logk2': sample[3],
-            'l1': -2*sigma_period,
-            'k1': np.pi # a joke, but it works
+            'logk1': sample[3],
+            'l_hidden': -2*sigma_period,
+            'k_hidden': np.pi # a joke, but it works
         }
         popn_parameter_list.append(parameters)
 

gyrointerp/models.py
@@ -11,13 +11,15 @@ Helper functions:
     logistic
     teff_zams
     teff_0
-    C_uniform
+    g_lineardecay
 """
 import os, pickle
 from gyrointerp.paths import DATADIR
 import pandas as pd, numpy as np
 from numpy import array as nparr
 from os.path import join
+
+from scipy.stats import norm, uniform
 
 ###########
 # helpers #
@@ -95,7 +97,7 @@ def teff_0(age, bounds_error='limit'):
     return teff0
 
 
-def C_uniform(age, bounds_error='limit', y0=1/2):
+def g_lineardecay(age, bounds_error='limit', y0=1/2):
     """
     How the third uniform component amplitude from slow_sequence_residual
     decreases with age Full amplitude at 120 Myr.  `y0` amplitude by 300 Myr
@@ -153,16 +155,12 @@ def slow_sequence_residual(
         * a gaussian in "y_grid" , with an age-varying cutoff in Teff, imposed
         as a logistic taper.
 
-        * (optional) an age-varying and Teff-varying uniform distribution,
-        multiplied by the inverse the gaussian's taper, and then truncated to
-        ensure that stars rotate faster than zero days, and to ensure that we
-        model only the fast sequence.  This uniform distribution is also
-        tapered by a logistic function at the "slow" end to yield a smoother
-        transition to the gaussian.
-
-        * an age-varying and Teff-invariant uniform distribution, similar to
-        the one described above, that accounts for the few fast-rotating
-        outliers by having a longer taper scale length.
+        * an age-varying and Teff-varying uniform distribution, multiplied by
+        the inverse of the gaussian's taper (but with independent scale
+        length), and then truncated to ensure that stars rotate faster than
+        zero days, and to ensure that we model only the fast sequence.  This
+        uniform distribution is also tapered by a logistic function at the
+        "slow" end to yield a smoother transition to the gaussian.
 
     Args:
         y_grid, teff_grid, poly_order, reference_model_ids, reference_ages:
@@ -175,9 +173,8 @@ def slow_sequence_residual(
         their sequence).
 
         popn_parameters: (str) "default", or (dict) containing the
-        population-level free parameters.  Keys of "A", "C", "C_y0", "k0",
-        "l1", "k1", and "k2" must all be specified.  If "B" is not specified,
-        assumes B=0, and the optional component mentioned above is omitted.
+        population-level free parameters.  Keys of "a0", "a1", "k0", "k1",
+        "y_g", "l_hidden", and "k_hidden" must all be specified.
 
     Returns:
         resid_y_Teff: 2d array with dimension (N_y_grid x N_teff_grid)
@@ -185,39 +182,25 @@ def slow_sequence_residual(
 
     assert len(reference_ages) == len(reference_model_ids)
 
-    from scipy.stats import norm, uniform
-
     # The intrinsic width (RMS) of the slow sequence, in units of days.
     sigma_period = 0.51
 
     if popn_parameters == "default":
         # from run_emcee_fit_gyro_model; MAP-values
         # [8.25637486, 0.6727635, -4.8845869, -6.23968718, -0.14829162]
-        A = 1
-        B = 0
-        C = 8.256
-        C_y0 = 0.673
+        a0 = 1
+        a1 = 8.256
+        y_g = 0.673
         k0 = np.e**-4.885
-        l1 = -2*sigma_period
-        k1 = np.pi # a joke, but it works
-        k2 = np.e**-6.240
-
+        k1 = np.e**-6.240
+        l_hidden = -2*sigma_period
+        k_hidden = np.pi # a joke, but it works
 
     elif isinstance(popn_parameters, dict):
 
-        A = popn_parameters["A"]
-
-        if "B" in popn_parameters:
-            B = popn_parameters["B"]
-        elif "logB" in popn_parameters:
-            B = np.exp(popn_parameters["logB"])
-        else:
-            if verbose:
-                print("B not explicitly set; assuming fully omitted.")
-            B = 0
-
-        C = popn_parameters["C"]
-        C_y0 = popn_parameters["C_y0"]
+        a0 = popn_parameters["a0"]
+        a1 = popn_parameters["a1"]
+        y_g = popn_parameters["y_g"]
 
         if "k0" in popn_parameters:
             k0 = popn_parameters["k0"]
@@ -225,16 +208,15 @@ def slow_sequence_residual(
             k0 = np.exp(popn_parameters["logk0"])
         else:
             raise NotImplementedError
-
-        l1 = popn_parameters["l1"]
-        k1 = popn_parameters["k1"]
-
-        if "k2" in popn_parameters:
-            k2 = popn_parameters["k2"]
-        elif "logk2" in popn_parameters:
-            k2 = np.exp(popn_parameters["logk2"])
+        if "k1" in popn_parameters:
+            k1 = popn_parameters["k1"]
+        elif "logk1" in popn_parameters:
+            k1 = np.exp(popn_parameters["logk1"])
         else:
             raise NotImplementedError
+
+        l_hidden = popn_parameters["l_hidden"]
+        k_hidden = popn_parameters["k_hidden"]
 
     else:
         raise NotImplementedError
@@ -271,7 +253,7 @@ def slow_sequence_residual(
 
     # Taper the "upper" part of the uniform ("fast rotator") distribution to
     # avoid overlap with the slow sequence.
-    taper_y = 1 - logistic(y_grid, l1, L=1, k=k1)
+    taper_y = 1 - logistic(y_grid, l_hidden, L=1, k=k_hidden)
 
     uniform_taper_y = uniform_y * taper_y
 
@@ -296,23 +278,22 @@ def slow_sequence_residual(
     # smoothing length.
     uniform_y_Teff_1 = 1.*uniform_y_Teff * (
         1-logistic(
-            teff_grid, teff_zams(age, bounds_error=bounds_error), L=1, k=k2
+            teff_grid, teff_zams(age, bounds_error=bounds_error), L=1, k=k1
         )
     )
 
-    A = A
-    B = B
-    C_prefactor = C * C_uniform(age, bounds_error=bounds_error, y0=C_y0)
+    a0 = a0
+    a1_prefactor = a1 * g_lineardecay(age, bounds_error=bounds_error, y0=y_g)
 
     # Initial iteration of model
-    resid_y_Teff_0 = A*gaussian_y_Teff + B*uniform_y_Teff_0 + C_prefactor*uniform_y_Teff_1
+    resid_y_Teff_0 = a0*gaussian_y_Teff + a1_prefactor*uniform_y_Teff_1
 
     # marginalize over y_grid
     resid_Teff_0 = np.trapz(resid_y_Teff_0, y_grid, axis=0)
 
     # normalize to ensure uniform distribution over Teff
     resid_y_Teff = (1/resid_Teff_0[None,:])*(
-        A*gaussian_y_Teff + B*uniform_y_Teff_0 + C_prefactor*uniform_y_Teff_1
+        a0*gaussian_y_Teff + a1_prefactor*uniform_y_Teff_1
     )
 
     return resid_y_Teff

gyrointerp/plotting.py
@@ -805,16 +805,15 @@ def plot_data_vs_model_prot(
             if ix % 8 == 0:
                 print(age, ix)
             sample = sel_samples[ix, :]
-            #C, C_y0, logk0, logk2, logf = theta
+            #a1, y_g, logk0, logk1, logf = theta
             popn_parameters = {
-                'A': 1,
-                'B': 0,
-                'C': sample[0],
-                'C_y0': sample[1],
+                'a0': 1,
+                'a1': sample[0],
+                'y_g': sample[1],
                 'logk0': sample[2],
-                'logk2': sample[3],
-                'l1': -2*sigma_period,
-                'k1': np.pi # a joke, but it works
+                'logk1': sample[3],
+                'l_hidden': -2*sigma_period,
+                'k_hidden': np.pi # a joke, but it works
             }
             h_vals_ss, h_vals_fs, teff_midway = _get_model_histogram(
                 age, popn_parameters=popn_parameters

commit 01428e60291d2a80f90f458f4a65150e628da001

[bug fix] run_emcee drivers
https://github.com/lgbouma/gyro-interp/commit/82d376007bd24bc2bc0638b760d8047ee86d2d56
introduced a typo from the original emcee run, because I played fast and
loose with copy-paste.

this commit corrects that typo.

drivers/run_crossvalidation_emcee_fit_gyro_model.py
@@ -4,8 +4,9 @@ Run MCMC to get best-fit a1, y_g, logk0, logk1, logf, and their uncertainties.
 "300-Myr", and "Praesepe" datasets, per the referee's suggestion).
 
 Usage:
-    First, runi
-    Check modelid and grid matches what you want.  Then:
+    First, run plot_crossvalidation_cdf_fast_slow_ratio.py.
+    Then:
+    Check that modelid matches what you want.  Then:
     $ python -u run_crossvalidation_emcee_fit_gyro_model.py &> logs/logname.log &
 """
 
@@ -93,10 +94,10 @@ def log_likelihood(theta):
         sigma_period = 0.51
         parameters = {
             'a0': 1,
-            'a1': sample[0],
-            'y_g': sample[1],
-            'logk0': sample[2],
-            'logk1': sample[3],
+            'a1': a1,
+            'y_g': y_g,
+            'logk0': logk0,
+            'logk1': logk1,
             'l_hidden': -2*sigma_period,
             'k_hidden': np.pi # a joke, but it works
         }
@@ -264,6 +265,8 @@ def main(xvalset_id):
         txt = "\mathrm{{{3}}} = {0:.3f}_{{-{1:.3f}}}^{{{2:.3f}}}"
         txt = txt.format(mcmc[1], q[0], q[1], labels[i])
         print(txt)
+
+    del datasets
 
 
 

drivers/run_emcee_fit_gyro_model.py
@@ -85,10 +85,10 @@ def log_likelihood(theta):
         sigma_period = 0.51
         parameters = {
             'a0': 1,
-            'a1': sample[0],
-            'y_g': sample[1],
-            'logk0': sample[2],
-            'logk1': sample[3],
+            'a1': a1,
+            'y_g': y_g,
+            'logk0': logk0,
+            'logk1': logk1,
             'l_hidden': -2*sigma_period,
             'k_hidden': np.pi # a joke, but it works
         }