c291078dc806691c48c65db54657a096b9ea187d
lgbouma <bouma.luke@gmail.com>
2024-05-29 17:48:35 -0700

    update init

--- gyrointerp/__init__.py
+++ gyrointerp/__init__.py
@@ -1,4 +1,11 @@
 __version__ = 0.3
+__uri__ = "https://gyro-interp.readthedocs.io/"
+__author__ = "Luke Bouma"
+__email__ = "bouma.luke@gmail.com"
+__license__ = "MIT"
+__description__ = (
+    'Gyrochronology by interpolation of open cluster rotation sequences.'
+)
 
 # the basic logging styles common to all gyro-interp modules
 log_sub = '{'
@@ -8,3 +15,10 @@ log_date_fmt = '%y%m%d %H:%M:%S'
 from gyrointerp.gyro_posterior import gyro_age_posterior
 from gyrointerp.gyro_posterior import gyro_age_posterior_list
 from gyrointerp.helpers import get_summary_statistics
+
+__all__ = [
+    "gyro_age_posterior",
+    "gyro_age_posterior_list",
+    "get_summary_statistics",
+    "__version__",
+]
